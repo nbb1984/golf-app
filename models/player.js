099ad2893fc0251ea69cef98f6f0167e2d728c33
nbb1984 <nicholas.bole@gmail.com>
2017-08-17 18:38:51 -0400

    Update player.js

--- models/player.js
+++ models/player.js
@@ -3,24 +3,27 @@ module.exports = function(sequelize, DataTypes) {
     
  var Player = sequelize.define("Player", {
 
-	   name: {
+	   player_name: {
 	        type: DataTypes.STRING,
-	        allowNull: false,
+	        allowNull: true,
 	        validate: {
 	            len: [1, 140]
 	        }
-	    }
+	    },
+	    email: DataTypes.STRING,
+	    password: DataTypes.STRING,
+	    team: DataTypes.STRING
 	});
 
 	 Player.associate = function(models) {
-	    // Associating Game with Posts
-	    // When an Author is deleted, also delete any associated Posts
-	    Player.hasOne(models.Player_To_Game, {
 
-		  foreignKey: {
-	        allowNull: false
-	      },
-	      onDelete: "cascade"
+	    Player.hasMany (models.PlayerToGame, {		
+			onDelete: "cascade"
+	    });
+
+	    Player.belongsToMany (models.Team, {
+			through: "playTeam",
+			onDelete: "cascade"
 	    });
 	  };
 	  return Player; 
