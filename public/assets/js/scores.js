530f0e48fa7ff3349db65db3e6ca78dabda97197
Julie Quinn <jmq2134@gmail.com>
2017-08-17 00:10:29 -0400

    scores.js working for functions 1 and 2

--- public/assets/js/scores.js
+++ public/assets/js/scores.js
@@ -1,102 +1,77 @@
-/// RUN EACH TIME NEW SCORE ENTERED BY ANY PLAYER (ON LOAD)
-$(document).ready(function() {
+ $(document).ready(function() {
 
-    $(".btn submit-scores-btn").click(function(event) {
-        playerJS();
-    });
+     /// FUNCTIONS
 
-}); // CLOSE DOCUMENT LOAD 
+     // (1) FOR EACH TEAM SCORECARD, FIND THE LOWEST SCORE BY A PLAYER FOR EACH HOLE
+     // ADD THE LOWEST SCORE TO THE TEAM'S ROW IN THE GROUP SCOREBOARD
 
-// ---------------------------- PLAYER JS ---------------------------- //
+     // (2) SUM THE TOTAL NUMBER OF STROKES PER HOLE
 
-var playerJS = function() {
-
-    // SUM TOTAL SCORE OF PLAYER'S STROKES
-    newSum(event);
-
-    // CHECK TO SEE IF ALL OTHER TEAMMATES HAVE ENTERED SCORE FOR THE HOLE
-
-        // IF ALL TEAMMATES HAVE ENTERED SCORE FOR HOLE
-
-            // FIND LOWEST SCORE
-
-            // ADD SCORE TO GROUP SCORECARD
-
-            // RUN GROUP JS 
-            groupJS();
-
-     // IF HOLE INCOMPLETE
-
-            // DO NOTHING
-
-}; // Close playerJS
+     // (3) FIND THE PLAYER/TEAM WITH THE LOWEST SCORE FOR ALL COMPLETED HOLES
 
 
+     // ---------------------------- (1) GROUP SCOREBOARD ---------------------------- //
 
-// ---------------------------- GROUP JS ---------------------------- //
+     // Find the lowest score from each team and input into the group scorecard
 
-var groupJS = function() {
+     var findWinner = function() {
 
-    // SUM TOTAL SCORE OF GROUP'S STROKES
-    newSum(event);
+         // For each playerTable
+         $("table[id*='playerTable']").each(function(i, a) {
+             console.log(a);
 
-    // FIND WHAT HOLE ALL GROUPS HAVE PLAYED THROUGH
-    findHoles();
-    console.log(col); // this should be the incomplete hole
+             // For each hole
+             $(a).find("tr:first-child>td:not('.rowsum')").each(function(j) {
+                 var j = j;
+                 var bestHoleScore = 100;
+                 // For each row (player) in the playerTable
+                 $(a).find(".scoreRow").each(function(k, c) {
+                     if (Number($(c).find("td:eq(" + j + ")>input")[0].value) > 0) {
+                         bestHoleScore = (Number($(c).find("td:eq(" + j + ")>input")[0].value) < bestHoleScore) ? $(c).find("td:eq(" + j + ")>input")[0].value : bestHoleScore;
+                     }
+                 });
+                 bestHoleScore = (bestHoleScore == 100) ? 0 : bestHoleScore;
+                 $("#groupTable>tbody>tr:eq(" + i + ")>td:eq(" + j + ")>input")[0].value = bestHoleScore;
+
+             });
+
+         })
+     }
+
+     findWinner(); 
 
 
-    // COMPARE TOTAL SCORE OF CURRENT HOLE PLAYED AND UPDATE WINNING TEAM IN GAME DB
+     // ---------------------------- (2) SUM ALL SCORE ROWS ---------------------------- //
+
+     // Sum all score rows
+
+     // Variables to run newSum
+     var row = 0,
+         col = 0,
+         ncol = 0;
+     var sum;
 
 
+     function newSum(event) {
 
-} // Close groupJS
+         // sum by row
+         $("tr").each(function(rowindex) {
+             sum = 0;
+             col = 0;
+             $(this).find("td").each(function(colindex) {
+                 col++;
+                 newval = $(this).find("input").val();
+                 if (isNaN(newval)) {
+                     $(this).html(sum);
+                     if (col > ncol) {
+                         ncol = col - 1
+                     }
+                 } else {
+                     sum += parseInt(newval);
+                 }
+             });
+         });
+     } // Close newSum
 
-
-
-
-// --------------------------- SUM STROKES -------------------------- //
-
-function newSum(event) {
-    var row = 0,
-        col = 0,
-        ncol = 0;
-    var sum;
-    // sum by row
-    $(".scoreRow").each(function(rowindex) {
-        sum = 0;
-        col = 0;
-        $(this).find("td").each(function(colindex) {
-            col++;
-            newval = $(this).find("input").val();
-            if (isNaN(newval)) {
-                $(this).html(sum);
-                if (col > ncol) {
-                    ncol = col - 1
-                }
-            } else {
-                sum += parseInt(newval);
-            }
-        });
-    });
-} // Close newSum
-
-// -------------------- FIND COMPLETE HOLES ------------------------ //
-
-function findHoles() {
-    for (col = 1; col < ncol + 1; col++) {
-        console.log("column: " + col);
-        sum = 0;
-        $("tr").each(function(rowindex) {
-            $(this).find("td:nth-child(" + col + ")").each(function(rowindex) {
-                newval = $(this).find("input").val();
-                console.log(newval);
-                if (isNaN(newval)) {
-                    console.log("column: " + col);
-                    return (col);
-                } else {
-                    sum += parseInt(newval);
-                }
-            });
-        });
-    }
-} // Close findHoles
+     newSum();
+ });
