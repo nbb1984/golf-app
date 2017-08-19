3c27df933ced7a329665af625ca271e301829676
Julie Quinn <jmq2134@gmail.com>
2017-08-18 21:33:54 -0400

    Added enter score route and changed the route on enterScores.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -65,7 +65,7 @@ module.exports = function(app) {
                     }
                 })
                 .then(function(data) {
-                var hbsObject = { PlayerToGame: data };
+                    var hbsObject = { PlayerToGame: data };
                     res.render("index", hbsObject);
                 });
         });
@@ -154,7 +154,6 @@ module.exports = function(app) {
 
                 }).then(function(dbP2G) {
 
-                    // res.json({ dbP2G, dbTeam, dbPlayer, dbGame });
                     res.redirect("/game/:" + dbP2G.GameId + "/player/:" + dbP2G.PlayerId);
 
                 }).catch(function(error) {
@@ -166,90 +165,116 @@ module.exports = function(app) {
     });
 
 
-
     /// ENTER SCORE TAB ON GAME
 
+    app.post("/api/enterscore/:PlayerId/:GameId", function(req, res) {
 
+        console.log("\n\n\n>>>>");
+        console.log(req.body);
+        console.log("\n\n\n>>>>");
 
+        console.log(req.res.PlayerId);
+        console.log(req.res.GameId);
 
+        db.PlayerToGame.update({
+
+            Hole1: req.body.Hole1,
+            Hole2: req.body.Hole2,
+            Hole3: req.body.Hole3,
+            Hole4: req.body.Hole4,
+            Hole5: req.body.Hole5,
+            Hole6: req.body.Hole6,
+            Hole7: req.body.Hole7,
+            Hole8: req.body.Hole8,
+            Hole9: req.body.Hole9,
+            Hole10: req.body.Hole10,
+            Hole11: req.body.Hole11,
+            Hole12: req.body.Hole12,
+            Hole13: req.body.Hole13,
+            Hole14: req.body.Hole14,
+            Hole15: req.body.Hole15,
+            Hole16: req.body.Hole16,
+            Hole17: req.body.Hole17,
+            Hole18: req.body.Hole18,
+
+        }, {
+            where: {
+                PlayerId: req.params.id,
+                GameId: req.params.id
+
+            }
+        }).then(function(data) {
+
+            res.redirect('//game/:GameId/player/:PlayerId"');
+
+        }).error(function(err) {
+            console.log("Update failed");
+        });
+        
+    });
 
     // ---------------------------- INCOMPLETE ---------------------------- //
 
-    // GETS users for handling log-in info. // may have to be restructured.
-    app.get("/api/login", function(req, res) {
+    // // GETS users for handling log-in info. // may have to be restructured.
+    // app.get("/api/login", function(req, res) {
 
-        db.Player.findOne({
-            where: {
-                name: req.body.name,
-                password: req.body.password,
-            }
-        })
+    //     db.Player.findOne({
+    //         where: {
+    //             name: req.body.name,
+    //             password: req.body.password,
+    //         }
+    //     })
 
-        db.Game.findOne({
-            where: {
-                id: req.params.id
-            },
-        }).then(function(dbPlayer) {
+    //     db.Game.findOne({
+    //         where: {
+    //             id: req.params.id
+    //         },
+    //     }).then(function(dbPlayer) {
 
-            if (username) {
-                res.redirect("/game/" + gameID + "/player/" + playerID)
-            }
+    //         if (username) {
+    //             res.redirect("/game/" + gameID + "/player/" + playerID)
+    //         }
 
-            res.json(dbPlayer);
-        });
-    });
-
-
-    // GETing specific info about a game.
-    app.get("/game/:gameID/player/:playerID", function(req, res) {
-        db.Game.findOne({
-            where: {
-                id: req.params.gameID
-            },
-            // include: [{
-            //     model: db.Team
-            // }]
-        }).then(function(dbGame) {
-            res.json(dbGame);
-        });
-
-        db.PlayerToGame.findAll({
-            where: {
-                GameID: req.params.gameID
-            }
-        }).then(function(dbPlayerToGame) {
-            res.json(dbPlayerToGame);
-        });
+    //         res.json(dbPlayer);
+    //     });
+    // });
 
 
-    });
+    // // GETing specific info about a game.
+    // app.get("/game/:gameID/player/:playerID", function(req, res) {
+    //     db.Game.findOne({
+    //         where: {
+    //             id: req.params.gameID
+    //         },
+    //         // include: [{
+    //         //     model: db.Team
+    //         // }]
+    //     }).then(function(dbGame) {
+    //         res.json(dbGame);
+    //     });
 
-    //
-    app.get("/find/game/:gameID/player/:playerID", function(req, res) {
-        db.Player_To_Game.findAll({
-            include: db.Player,
-            include: db.Team,
-            where: {
-                GameID: req.params.gameID
-            }
-        }).then(function(dbPlayerToGame) {
-            res.json(dbPlayerToGame);
-        });
-    });
-
-    app.post("/api/enterscore/:hole", function(req, res) {
-        var scoreOfHole = {}
-        scoreOfHole[req.body.hole] = req.body.score;
-        db.PlayerToGame.update(scoreOfHole).then(function(dbPlayerToGame) {
-
-            res.json(dbPlayerToGame);
-        }).catch(function(error) {
-            res.send(error);
-        });
-
-    });
+    //     db.PlayerToGame.findAll({
+    //         where: {
+    //             GameID: req.params.gameID
+    //         }
+    //     }).then(function(dbPlayerToGame) {
+    //         res.json(dbPlayerToGame);
+    //     });
 
 
+    // });
 
+    // //
+    // app.get("/find/game/:gameID/player/:playerID", function(req, res) {
+    //     db.Player_To_Game.findAll({
+    //         include: db.Player,
+    //         include: db.Team,
+    //         where: {
+    //             GameID: req.params.gameID
+    //         }
+    //     }).then(function(dbPlayerToGame) {
+    //         res.json(dbPlayerToGame);
+    //     });
+    // });
 
-}
+} // CLOSE EXPORT
